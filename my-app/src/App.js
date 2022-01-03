29958e39b48cf64d12adda25465d6f6a282e29ff
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2022-01-02 21:34:03 -0500

    immutable states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,80 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+let i = 0;
 
 function App() {
+  // ----------- bad practice
+  const [array, setArray] = useState([]);
+  const [obj,setObj] =useState({});
+
+  // const addNumberToArray = () => {
+  //   array.push(i);
+  //   i++;
+  //   console.log(array);
+  //   setArray(array);
+  // }
+  // ----------- good practice
+
+  const addNumberToArray = () => {
+    // first make a copy and add the new element
+    const arrayCopy = [...array, i];
+    i++;
+    console.log(i);
+    setArray(arrayCopy);
+    // ...array to copy the original one, and
+  };
+
+  const addItemToCarts = () => {
+    const item = 'toilet paper'; 
+    const objCopy = {...obj}; 
+    objCopy[i] = item; 
+    i++;
+    setObj(objCopy);
+  }
   return (
     <div className="App">
+      <h2>{array}</h2>
+      <button onClick={addNumberToArray}>click me to add to array</button>
+
+      {/* <h2>{Object.keys(obj)}, {Object.values(obj)}</h2> */}
+      {/* JSON.stringify: converts a JavaScript object or value to a JSON string  */}
+      <h2>{JSON.stringify(obj)}</h2>
+
+    {/* Event listener: onClick */}
+      <button onClick={addItemToCarts}>click me to add to cart</button>
     </div>
   );
 }
+
+// function App() {
+//   // -------------did not render!!
+//   let badnum = 1;
+//   const badAddNum = () => {
+//     badnum += 1;
+//     console.log(badnum);
+//   };
+
+//   // ---------------- use useState to tell react to re-render!!
+//   const [num, setNum] = useState(1);
+
+//   const addNum = () => {
+//     setNum(num + 1);
+//   };
+
+//   console.log("re-rendering!!!!!! ");
+
+//   return (
+//     <div className="App">
+//       {badnum}
+//       <button onClick={badAddNum}> click me</button>
+
+//       <br />
+//       {num}
+//       <button onClick={addNum}> click me</button>
+//     </div>
+//   );
+// }
 
 export default App;
--- my-app/src/immutable_test.js
+++ my-app/src/immutable_test.js
@@ -1,1 +1,62 @@
-console.log("what");
+// ----------------------- objects and arrays always pass by reference 
+
+// // string 
+// let fruit = 'banana'; 
+// console.log('fruit:', fruit); 
+// let fruitCopy = fruit; 
+// console.log('fruitCopy:', fruitCopy); 
+
+// fruit= 'apple'; 
+// console.log('fruit:', fruit); 
+// console.log('fruitCopy:', fruitCopy); // still banana 
+
+
+// // object 
+// let car = {make:'toyota', year: 2021, 'model': 'gp', color: 'blue'}
+// console.log('car:', car); 
+// let carCopy = car; 
+// console.log('carCopy:', carCopy); 
+
+// car.color= 'red'; 
+// console.log('car:', car); 
+// console.log('carCopy:', carCopy); // changed color as well
+
+// // Array
+// let list = [1,2,3]
+// console.log('list:', list); 
+// let listCopy = list; 
+// console.log('listCopy:', listCopy); 
+
+// list[2]= 100; 
+// console.log('list:', list); 
+// console.log('listCopy:', listCopy); // changed as well, pointing to the same location
+
+//------------ spread operator / three dot notation 
+
+// let car = {make:'toyota', year: 2021, 'model': 'gp', color: 'blue'}
+// console.log('car:', car); 
+// let carCopy = {...car}; 
+// console.log('carCopy:', carCopy); 
+
+// car.color= 'red'; 
+// console.log('car:', car); 
+// console.log('carCopy:', carCopy); // did not change color , pointing to different things 
+
+// but it wont solve within
+// let car = {make:'toyota', year: 2021, 'model': 'gp', color: 'blue', trims: ['plastic', 'gold'] }
+// console.log('car:', car); 
+// let carCopy = {...car}; 
+// console.log('carCopy:', carCopy); 
+
+// car.trims.push('leather'); 
+// console.log('car:', car); 
+// console.log('carCopy:', carCopy); // both changed
+
+// let car = {make:'toyota', year: 2021, 'model': 'gp', color: 'blue', trims: ['plastic', 'gold'] }
+// console.log('car:', car); 
+// let carCopy = {...car, trims: [...car.trims]}; // copy array within the object as well.
+// console.log('carCopy:', carCopy); 
+
+// car.trims.push('leather'); 
+// console.log('car:', car); 
+// console.log('carCopy:', carCopy); // both changed
